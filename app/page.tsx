333ae3472f899dbb6d7b32be341a424d34e811fc
Tad Yuh <tadyuh76@gmail.com>
2024-07-10 18:11:52 +0700

    first init & background image implement

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen pt-[100px] bg-[url('/background-pattern.png')] bg-repeat">
+      <div className=""></div>
+    </main>
+  )
+}
