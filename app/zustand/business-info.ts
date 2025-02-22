3ef771edb1fd9bf5595674f8cfdde89cc2203a16
Tad Yuh <tadyuh76@gmail.com>
2025-02-22 09:53:04 +0700

    redeploy

--- app/zustand/business-info.ts
+++ app/zustand/business-info.ts
@@ -1,14 +1,14 @@
-import { create } from 'zustand'
-import { IBusinessInfo } from '../[slug]/page'
+import { create } from "zustand";
+import { IBusinessInfo } from "../[slug]/page";
 
 interface BusinessInfoState {
-  businessInfo: IBusinessInfo | null
-  setBusinessInfo: (info: IBusinessInfo) => void
+  businessInfo: IBusinessInfo | null;
+  setBusinessInfo: (info: IBusinessInfo) => void;
 }
 
-const useStore = create<BusinessInfoState>(set => ({
+const useStore = create<BusinessInfoState>((set) => ({
   businessInfo: null,
   setBusinessInfo: (info: IBusinessInfo) => set({ businessInfo: info }),
-}))
+}));
 
-export default useStore
+export default useStore;
